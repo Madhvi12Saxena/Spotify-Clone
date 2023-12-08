215154f507bfcb8f7c9ec9b382179c3d29a2d43f
Madhvi12Saxena <ssms122005@gmail.com>
2023-12-08 15:24:53 +0530

    Update

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ console.log("Welcome to Spotify");
 
 // Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('songs/.1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementById('songItem'));
 
 let songs = [
     {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
@@ -20,6 +21,11 @@ let songs = [
     {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
 ]
 
+songItems.forEach((element, i) => {
+    console.log(element, i);
+    element.getElementByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementByClassName("songName")[0].innerText = songs[i].songName; 
+});
 
 // audioElement.play();
 
