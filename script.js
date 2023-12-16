04d6c7bc5e864f9f831a45a3fc7fb58cb71c9a70
Madhvi12Saxena <ssms122005@gmail.com>
2023-12-16 21:59:23 +0530

    Update

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ let gif = document.getElementById('gif');
 let songItems = Array.from(document.getElementById('songItem'));
 
 let songs = [
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Ram Siya Ram", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Ram Siya Ram", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i) => {
@@ -66,13 +66,24 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
         // console.log(e.target);
         makeAllPlays();
-        index = parseInt(e.target.id);
+        songIndex = parseInt(e.target.id);
         e.target.classList.remove('fa-circle-play');
         e.target.classList.add('fa-circle-pause');
-        audioElement.src = `songs/${index+1}.mp3`;
+        audioElement.src = `songs/${songIndex+1}.mp3`;
         audioElement.currentTime = 0;
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
     })
+})
+
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>9){
+        songIndex = 0
+    }
+    else{
+        
+    }
+    songIndex += 1;
+
 })
